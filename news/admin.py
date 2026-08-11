150fe11d58fe418b155669ea253726ccbc4cd453
Atai Borubaev <tenguosu@gmail.com>
2026-08-11 23:20:56 +0600

    Настроил админ-панель для news

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import Category, Post
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
