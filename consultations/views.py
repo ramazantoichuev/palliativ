13c0e671636a089596cf25d8bf1624e5612657cc
Ekaterina Gudkova <katiegudkova2009@gmail.com>
2026-08-12 22:57:28 +0600

    ConsultationCreateView

--- consultations/views.py
+++ consultations/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import ConsultationRequest
+from .forms import ConsultationForm
+
+class ConsultationCreateView(CreateView):
+    model = ConsultationRequest
+    form_class = ConsultationForm
+    template_name = 'consultations/consultation.html'
+    success_url = reverse_lazy('consultations:success')
+
 
 # Create your views here.
