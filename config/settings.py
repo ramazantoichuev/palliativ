30908ac485ed3bbf11fcb5f2aa8362aee3cb3eef
Ekaterina Gudkova <katiegudkova2009@gmail.com>
2026-08-12 23:34:32 +0600

    Переводы ky/en для формы консультации

--- config/settings.py
+++ config/settings.py
@@ -130,9 +130,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locale'),
-]
+LOCALE_PATHS = [BASE_DIR / 'locale']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
