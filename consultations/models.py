8547e7666f1090f3539cc1be862be4d5260635fe
Ekaterina Gudkova <katiegudkova2009@gmail.com>
2026-08-12 22:40:13 +0600

    Модель ConsultationRequest

--- consultations/models.py
+++ consultations/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class ConsultationRequest(models.Model):
+    TOPIC_CHOICES = [
+        ('medical_help', _('Медицинская помощь')),
+        ('cooperation', _('Сотрудничество')),
+        ('partnership', _('Партнерство')),
+    ]
+
+    first_name = models.CharField(_('Имя'), max_length=255)
+    phone = models.CharField(_('Телефон'), max_length=20)
+    topic = models.CharField(_('Тема обращения'), max_length=20, choices=TOPIC_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} — {self.get_topic_display()}"
 
 # Create your models here.
