9083b6cf37c75419d066bb10d27400d57f8cd41f
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-08-15 14:15:04 +0600

    изменен путь в урле на main

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path('', include('main.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
