ae165ca077aae31c66eae4e1ef63e0caebbc7caa
Ekaterina Gudkova <katiegudkova2009@gmail.com>
2026-08-22 23:04:21 +0800

    D

---
+++ patients/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import DoctorPatientListView, PatientCardDetailView
+
+app_name = 'patients'
+
+urlpatterns = [
+    path('my/', DoctorPatientListView.as_view(), name='doctor_dashboard'),
+    path('my-card/', PatientCardDetailView.as_view(), name='patient_dashboard'),
+]
