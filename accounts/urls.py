7f1d2643ce3ea732be507e686ba5a74e74024af6
Ramazan <ramazantoichuev16@gmail.com>
2026-08-23 20:57:36 +0600

    добавил папку для тестов моделей

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from .views import CustomLoginView, WaitingApprovalView, PatientRegisterView, DoctorRegisterView
+
 
 app_name = 'accounts'
 
@@ -8,5 +10,6 @@ urlpatterns = [
     path('register/patient/', PatientRegisterView.as_view(), name='patient_register'),
     path('register/doctor/', DoctorRegisterView.as_view(), name='doctor_register'),
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('waiting-approval/', WaitingApprovalView.as_view(), name='waiting_approval'),
 ]
---
+++ patients/tests/test_models/test_patient_card.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+
+class PatientCard(TestCase):
+    def
