ac203c0fa48e26ce96e9a570828f8ac04fa64a7f
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-09-06 22:13:31 +0600

    В новости, ресурсы и мероприятия добавлен класс Sitemap, который собирает объекты модели и подсказывает роботу их приоритет и частоту проверки, а также когда эта страница менялась или создалась

---
+++ events/sitemaps.py
@@ -0,0 +1,13 @@
+from django.contrib.sitemaps import Sitemap
+from .models import Event
+
+
+class EventSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.7
+
+    def items(self):
+        return Event.objects.all()
+
+    def lastmod(self, obj):
+        return obj.created_at
