1b5a9ae1c83e80fb7839bc8e4b7269648227928e
Ekaterina Gudkova <katiegudkova2009@gmail.com>
2026-09-10 16:16:16 +0600

    Поддержка формата youtube.com/live/ в get_embed_url

--- resources/models/resources.py
+++ resources/models/resources.py
@@ -107,7 +107,7 @@ class ResourceVideoLink(models.Model):
 
     def get_embed_url(self):
         patterns = [
-            r'(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/embed\/)([a-zA-Z0-9_-]{11})',
+            r'(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/embed\/|youtube\.com\/live\/)([a-zA-Z0-9_-]{11})',
         ]
         for pattern in patterns:
             match = re.search(pattern, self.url)
