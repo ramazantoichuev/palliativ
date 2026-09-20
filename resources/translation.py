b91b44b45142085d58f01118ddb06363ee0701d3
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-09-20 19:21:01 +0600

    Добавлен функционал для перевода

--- resources/translation.py
+++ resources/translation.py
@@ -1,4 +1,5 @@
 from modeltranslation.translator import TranslationOptions, register
+from models.terms import Term
 
 from .models.resources import Resource
 
@@ -6,3 +7,7 @@ from .models.resources import Resource
 @register(Resource)
 class ResourceTranslationOptions(TranslationOptions):
     fields = ("title", "description")
+
+@register(Term)
+class TermTranslationOptions(TranslationOptions):
+    fields = ('name', 'definition')
